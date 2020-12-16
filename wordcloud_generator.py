09550d5df3ffeb62124ffc1ed3f5124a432f3f67
Mirror2529156651 <42204820+Mirror2529156651@users.noreply.github.com>
2020-12-16 18:01:21 +0800

    update

--- wordcloud_generator.py
+++ wordcloud_generator.py
@@ -1,8 +1,10 @@
 import jieba
 import matplotlib.pyplot as plt
-#from wordcloud import WordCloud
+from wordcloud import WordCloud,ImageColorGenerator
+from PIL import Image
 from collections import Counter
-import stylecloud
+import numpy as np
+#import stylecloud
 
 jieba.set_dictionary('dict.txt.big')
 s=''
@@ -16,11 +18,21 @@ with open('stopword.txt','r',encoding='utf-8') as f:
 for i in l:
     s+=i
 breakword=jieba.cut(s)
-with open('tmp.txt','w',encoding='utf-8') as f:
+'''with open('tmp.txt','w',encoding='utf-8') as f:
     f.write('\n'.join(breakword))
 stylecloud.gen_stylecloud(file_path='tmp.txt',
                           icon_name='fas fa-cookie',
                           background_color='white',
                           output_name='t.png',
                           font_path=r'C:\Windows\Fonts\mingliu.ttc',
-                          custom_stopwords=sp)
+                          custom_stopwords=sp)'''
+for word in breakword:
+    if word not in sp:
+        dic.append(word)
+dic=Counter(dic)
+mask=np.array(Image.open('ramen.png'))
+wordcloud=WordCloud(background_color='white',mask=mask,font_path=r'C:\Windows\Fonts\mingliu.ttc')
+wordcloud.generate_from_frequencies(dic)
+image_colors=ImageColorGenerator(mask)
+wordcloud.recolor(color_func=image_colors)
+wordcloud.to_file('t.png')
